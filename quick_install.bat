f58e328cc8a79a1aae2527d5a7cdd6a00f65b608
TrippleAWap <90356816+TrippleAWap@users.noreply.github.com>
2023-11-06 15:31:10 -0700

    Update quick_install.bat

--- quick_install.bat
+++ quick_install.bat
@@ -1,4 +1,4 @@
 @echo off
-git clone https://github.com/TrippleAWap/LoseAnotherRealm.git
+git clone https://github.com/TrippleAWap/LoseAnotherRealm.git TrippleAWap-LoseAnotherRealm
 npm install
 @echo Installation Complete, edit the configuration in config.json
