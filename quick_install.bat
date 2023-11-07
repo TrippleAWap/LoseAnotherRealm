d45bbd9706557a407999523e82c54c0f8c5a6413
TrippleAWap <90356816+TrippleAWap@users.noreply.github.com>
2023-11-06 21:43:33 -0700

    Delete quick_install.bat

--- quick_install.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-git clone https://github.com/TrippleAWap/LoseAnotherRealm.git TrippleAWap-LoseAnotherRealm
-npm install
-@echo Installation Complete, Starting Application.
-cls && node index.js
